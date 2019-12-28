5f76223a537532276c6fd78266eaa7cf240b3eab
Olga Pichuzhkina <olga-p-98@mail.ru>
2019-12-28 08:52:25 +0300

    added comprehensive readme

---
+++ db_init.py
@@ -0,0 +1,16 @@
+import logging
+import pandas as pd
+from tqdm import tqdm
+from database import Database
+
+logging.getLogger().setLevel(logging.INFO)
+
+db = Database()
+logging.info("Creating the corpora table...")
+db.execute("""
+           CREATE TABLE corpora
+           (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+           lang TEXT NOT NULL,
+           texts_number INTEGER);
+           """)
+logging.info("Table creation finished.")
